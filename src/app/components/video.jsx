628010ec85045c282a42aff33e5192e3389637ad
sidmaz666 <sidmazumder8@gmail.com>
2022-03-12 21:56:51 +0530

    allow fullscreen

--- src/app/components/video.jsx
+++ src/app/components/video.jsx
@@ -8,7 +8,6 @@ function Video(props){
 		src={props.link}
 		className="w-full h-[40rem] md:h-[50rem]"
 		frameBorder="0"
-	        allow="autoplay; fullscreen; picture-in-picture" 
 		allowFullScreen>
 	      </iframe>
 	      </div>
